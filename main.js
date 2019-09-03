c23e4dffee410c6abcf0314b27b3cbf0e41371c1
신현일/클라우드개발센터/NE <hyunil.shin@nhnent.com>
2019-09-03 18:17:27 +0900

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("jenkins + kubernetes test\n");
+  response.end("jenkins + kubernetes test222\n");
 });
 
 // listen on localhost:8000
